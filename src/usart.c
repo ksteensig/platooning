cbd55ab68624acd70c91594901d3e828328fc4a8
Kasper Steensig <kasper.steensig@gmail.com>
2018-10-06 15:07:34 +0200

    Initial build tool commit

---
+++ src/usart.c
@@ -0,0 +1,12 @@
+#include <libopencm3/cm3/dwt.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/scs.h>
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+#include "usart.h"
+
+int usart(void) {}
