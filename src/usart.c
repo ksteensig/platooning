33aad2c9a1aba8f31f771eff4e2c3ec0f44007b7
Kasper Steensig <kasper.steensig@gmail.com>
2018-10-07 21:12:35 +0200

    Add full FreeRTOS support and use it together with libopencm3 usart

--- src/usart.c
+++ src/usart.c
@@ -1,32 +1,6 @@
-#include <libopencm3/cm3/dwt.h>
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/scs.h>
-#include <libopencm3/stm32/dma.h>
-#include <libopencm3/stm32/flash.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/usart.h>
-
 #include "usart.h"
 
 int usart(void) {
-  rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-  rcc_periph_clock_enable(RCC_GPIOA);
-  rcc_periph_clock_enable(RCC_USART2);
-  rcc_periph_clock_enable(RCC_DMA1);
-
-  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
-  gpio_set_af(GPIOA, GPIO_AF7, GPIO2 | GPIO3);
-
-  usart_set_baudrate(USART2, 115200);
-  usart_set_databits(USART2, 8);
-  usart_set_stopbits(USART2, USART_STOPBITS_1);
-  usart_set_mode(USART2, USART_MODE_TX_RX);
-  usart_set_parity(USART2, USART_PARITY_NONE);
-  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
-
-  usart_enable(USART2);
-
   unsigned char* str = "Hello world";
   int i;
 
